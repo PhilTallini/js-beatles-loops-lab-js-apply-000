e1879ad32ab6d1c0b38d14e216c4b2c214695707
Phil Tallini <philiptallini@gmail.com>
2018-12-28 01:30:38 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,5 +3,8 @@ function theBeatlesPlay(musicians, instruments) {
   var newArr = [];
   for (var i = 0; i < musicians.length; i++) {
     var musician = musicians[i];
+    var instrument = instruments[i];
+    newArr.push(musician + ' plays ' + instrument);
   }
+  return newArr;
 }
